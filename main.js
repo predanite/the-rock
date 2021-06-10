bcf115150c1a358d8e2e2df383b3a99a789be5bc
predanite <74647753+predanite@users.noreply.github.com>
2021-06-10 19:50:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,25 @@ recognition.onresult = function run(event)
 {
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").value = Content ;
+    speak();
 }
+
+
+
+function speak ()
+{
+var synth = window.SpeechSynthesis;
+speakdata = document.getElementById("textbox").value;
+var utterthis =new SpeechSynthesisUtterance("speakdata");
+synth.speak(utterthis) ;
+Webcam.attach(camera);
+}
+
+
+webcam.set({
+    width:320,
+    height:240,
+    image_format : 'png',
+        png_quality :100
+});
+camera = document.getElementById("camera");
